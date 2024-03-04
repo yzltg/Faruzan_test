f9dfc07ef2fc013d3ac42ae573d19726df0fc4c8
FRZ <1801534592@qq.com>
2024-03-04 13:00:49 +0800

    P1020

--- luogu/train/yellow/P1020/P1020.cpp
+++ luogu/train/yellow/P1020/P1020.cpp
@@ -1,41 +1,36 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-const int INF=2147483647,MAXN=1e5+3;
-int n,t,H[MAXN],F[MAXN];
+const int INF=2147483647,N=1e5+5;
+int high[N],f[N],f1[N],length,max_len,times;
 void solve(){
-    while(cin>>H[++n]);
-    --n;
-    t=0;
-    memset(F,0,sizeof(F));
-    F[0]=INF;
-    for(int i=1;i<=n;i++){
-        int l=0,r=t+1;
+    while(cin>>high[++length]);
+    length--;
+    f[0]=INF;
+    for(int i=1;i<=length;i++){
+        int l=0,r=max_len+1;
         while(r-l>1){
-            int m=l+(r-l)/2;
-            if(F[m]>=H[i]) l=m;
-            else r=m;
+            int mid=l+r>>1;
+            if(high[i]<=f[mid])l=mid;
+            else r=mid;
         }
         int x=l+1;
-        if(x>t) t=x;
-        F[x]=H[i];
+        if(x>max_len) max_len=x;
+        f[x]=high[i];
     }
-    cout<<t<<"\n";
-    t=0;
-    memset(F,0,sizeof(F));
-    F[0]=0;
-    for(int i=1;i<=n;i++){
-        int l=0,r=t+1;
+    cout<<max_len<<"\n";
+    for(int i=1;i<=length;i++){
+        int l=0,r=times+1;
         while(r-l>1){
-            int m=l+(r-l)/2;
-            if(F[m]<H[i]) l=m;
-            else r=m;
+            int mid=l+r>>1;
+            if(high[i]>f1[mid])l=mid;//当高度大于f1[i]时,应在右边找,否则应在左边查找
+            else r=mid;
         }
         int x=l+1;
-        if(x>t) t=x;
-        F[x]=H[i];
+        if(x>times) times=x;
+        f1[x]=high[i];
     }
-    cout<<t;
+    cout<<times;
 }
 int main(){
     ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
